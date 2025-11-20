e4993d5b600e4bd748ae80d2ca00ffe184cdecd6
Skye Glitch <yourgithub@email.com>
2025-11-20 16:39:09 +0300

    update

--- .next/dev/server/chunks/ssr/_next-internal_server_app_prioritizer_page_actions_821dd2f3.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/prioritizer/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_prioritizer_page_actions_821dd2f3.js.map
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UmtjKkCFnEbpBRkR/BOgvprKVHMA55AbBf8SHpH1+HM=\"\n}"
